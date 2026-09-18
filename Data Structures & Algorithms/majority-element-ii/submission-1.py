de0b8ba2e85b49b814773689d523cda8b9ee2c18
Yuvraj Shekhar <syuvraj1999@gmail.com>
2026-09-18 15:06:17 +0200

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = {}
+        output = []
+        n = len(nums)/3
+        if n<1:
+            return nums
+        for num in nums:
+            if num in count:
+                count[num] += 1
+                if count[num]>n and num not in output:
+                    output.append(num)
+            else:
+                count[num] = 1
+
+        return output
+        
+        
